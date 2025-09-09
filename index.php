b5ab2b63fddbba5485cc660c2cd3d1683983ac2c
Mikk-Gregor Muuga <gregormikk@gmail.com>
2025-09-09 11:23:30 +0300

    Additional loops

--- index.php
+++ index.php
@@ -1,59 +1,19 @@
 <?php
 
-function hello (){
-    var_dump("Hello World");
-}
-
-hello();
-hello();
-hello();
-
-function helloName ($name){
-    var_dump('Hello World', $name);
-}
-
-helloName("MIkk");
-helloName("Muigab");
-helloName();
-
-function helloNameAndAge($name = 'unknown', $age){
-    var_dump("Hello $name and yout $age years old");
-}
-
-helloNameAndAge("Mikk", 23);
-helloNameAndAge("Muigab", 5);
-
-function stuff(...$args){
-    var_dump(...$args);
-}
-
-stuff(1,2,3);
-stuff(1,2,3,4,5,6,7);
-
-function sum($a, $b){
-    return $a + $b;
-    var_dump("This will never be executed");
-}
-
-$answer = sum(1,5);
-var_dump($answer);
-
-function biggerOrSmaller($a){
-    if($a > 10){
-        return 'Bigger';
-    } else {
-        return 'Smaller';
+for ($i = 0; $i < 10; $i++) {
+    if($i == 3){
+        continue;
     }
-}
-
-var_dump(biggerOrSmaller(15));
-var_dump(biggerOrSmaller(5));
-
-function recursive($i){
-    if($a < 10){
-        var_dump($i);
-        recursive($i + 1);
+    if($i==5){
+        break;
     }
+    var_dump($i);
 }
 
-recursive(0);
+for($i=0;$i<10;$i++){
+    for($j=0;$j<10;$j++){
+            break 2;
+        var_dump($i,$j);
+    }
+    var_dump($i, $j);
+}
