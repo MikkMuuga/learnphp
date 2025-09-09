4b7b85f8afd79a8dccc45e0fd7da54b429938288
Mikk-Gregor Muuga <gregormikk@gmail.com>
2025-09-09 13:38:02 +0300

    Add interface example

--- index.php
+++ index.php
@@ -1,95 +1,45 @@
 <?php
 
+////// library
 
-class cat {
-    use Colorful;
-}
-class Box {
-    use Colorful;
-    private $width;
-    protected $heigth;
-    private $length;
-
-        public static $count = 0;
-
-         public static function test(){
-            //var_dump($this -> width);
-            var_dump(static::class);
-         }
-    
-    public function __construct($w=0, $h=0, $l=0)
-    {   
-        self::$count++;
-        $this->width = $w;
-        $this->heigth = $h;
-        $this->length = $l;
-
-    }
-
-    public function getWidth(){
-        return $this->width;
-    }
-
-    public function setWidth($width){
-        if($width > 0 && is_numeric($width)){
-            $this->width = $width;
-        } else {
-            throw new Exception("Bro are you mad??");
+class Job {
+    public function task (Logger $logger){
+        for($i=0; $i<10; $i++){
+            $logger->log("task $i done");
         }
-
-    }
-
-    public function volume(){
-        return $this-> width * $this->heigth * $this->length;
     }
 }
 
-class MetalBox extends box {
-    public $material = 'metal';
-    public $massPerUnit = 2;
-
-    public function changeWidth(){
-        $this -> width = 333;
-    }
-
-    public function mass(){
-        return $this->volume() * $this->massPerUnit;
+class ConsoleLogger implements Logger{
+    public function log($msg){
+        echo $msg, "\n";
     }
 }
 
-trait Colorful {
-    private $color;
+interface Logger {
+    public function log($msg);
+}
 
-    public function setColor($color){
-        $this->color = $color;
-    }
-    public function getColor(){
-        return $this->color;
+
+
+////// library user code
+
+class nothinLogger implements Logger {
+    public function log($msg){
     }
 }
 
-$metalBox = new Box(2,3,4);
+class fileLogger {
+    public function log($msg){
+        $file = fopen("log.txt", "a");
+        fwrite($file, $msg."\n");
+        fclose($file);
+    }
+}
 
-$box2 = new Box(5,6,7);
-box::test();
-MetalBox::test();
-var_dump(Box::$count, Box::$count);
-
-
-//$box2 = new Box(5,6,7);
-//var_dump($metalBox::$count, $box2::$count);
-//$metalBox->width = 'cool value';
-$metalbox->setWidth(12);
-//$metalBox->width = 123;
-//$metalBox -> changeWidth();
-//var_dump($metalBox->width);
-
-var_dump($metalBox);
-var_dump($metalBox->volume());
-var_dump($metalBox->mass());
-
-
-
+$job = new Job();
+$logger = new NothingLogger();
+$job->task($logger);
 
 
 
