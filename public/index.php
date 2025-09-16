8ffa0828ce7cb12c31810319159f4fc6806e20c5
Mikk-Gregor Muuga <gregormikk@gmail.com>
2025-09-16 11:40:14 +0300

    Add simple router

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
 <?php
+
+echo '<code>';
+var_dump($_SERVER);
+echo '</code>';
+
+switch($_SERVER['REQUEST_URI']) {
+    case '/':
+        include __DIR__ . '/../views/index.php';
+        break;
+    case '/us':
+        include __DIR__ . '/../views/us.php';
+        break;
+    default:
+    echo '404  page not found'
+}
